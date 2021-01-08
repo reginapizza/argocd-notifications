c74eabc8bb19ebc11fd6d82ebdff49fd36f32003
Regina Scott <rescott@redhat.com>
2021-01-07 22:22:19 -0500

    .

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -16,6 +16,12 @@ setTimeout(function() {
             container.className = classes.join(' ');
         });
     }
+    
+    var script = document.createElement('script');
+    script.src = 'https://regina-argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
+        'callback=' + callbackName + '&project=regina-argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
+    
+    document.getElementsByTagName('head')[0].appendChild(script); 
 
     window.addEventListener("DOMContentLoaded", function() {
         var xhr = new XMLHttpRequest();
@@ -31,10 +37,4 @@ setTimeout(function() {
         };
         xhr.send();
     }); 
-    
-    var script = document.createElement('script');
-    script.src = 'https://regina-argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
-        'callback=' + callbackName + '&project=regina-argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
-    
-    document.getElementsByTagName('head')[0].appendChild(script); 
 }, 50);
