ac6a3ae0b5ce445402dbe4903cd0b1c5ac77d158
Regina Scott <rescott@redhat.com>
2021-01-21 15:54:02 -0500

    .

--- docs/js/version-select.js
+++ docs/js/version-select.js
@@ -18,30 +18,30 @@ window.addEventListener("DOMContentLoaded", function() {
     return select;
   }
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", ABS_BASE_URL + "/../versions.json");
-  xhr.onload = function() {
-    var versions = JSON.parse(this.responseText);
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("GET", ABS_BASE_URL + "/../versions.json");
+  // xhr.onload = function() {
+  //   var versions = JSON.parse(this.responseText);
 
-    var realVersion = versions.find(function(i) {
-      return i.version === CURRENT_VERSION ||
-             i.aliases.includes(CURRENT_VERSION);
-    }).version;
+  //   var realVersion = versions.find(function(i) {
+  //     return i.version === CURRENT_VERSION ||
+  //            i.aliases.includes(CURRENT_VERSION);
+  //   }).version;
 
-    var select = makeSelect(versions.map(function(i) {
-      return {text: i.title, value: i.version};
-    }), realVersion);
-    select.addEventListener("change", function(event) {
-      window.location.href = ABS_BASE_URL + "/../" + this.value;
-    });
+  var versions = (window[READTHEDOCS_DATA]).version
 
-    var container = document.createElement("div");
-    container.id = "version-selector";
-    container.className = "md-header-nav";
-    container.appendChild(select);
+  var select = makeSelect(versions.map(function(i) {
+    return {text: i.title, value: i.version};
+  }), realVersion);
+  select.addEventListener("change", function(event) {
+    window.location.href = ABS_BASE_URL + "/../" + this.value;
+  });
 
-    var header = document.querySelector(".md-header-nav > .md-header-nav__title");
-    header.parentNode.insertBefore(container, header.nextSibling);
-  };
-  xhr.send();
+  var container = document.createElement("div");
+  container.id = "version-selector";
+  container.className = "md-header-nav";
+  container.appendChild(select);
+
+  var header = document.querySelector(".md-header-nav > .md-header-nav__title");
+  header.parentNode.insertBefore(container, header.nextSibling);
 });
