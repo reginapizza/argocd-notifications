d076119052c5aa9b48153667149d8bab49fd223c
Regina Scott <rescott@redhat.com>
2021-01-21 09:47:53 -0500

    .

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -2,45 +2,45 @@ setTimeout(function() {
     const callbackName = 'callback_' + new Date().getTime();
     window[callbackName] = function (response) {
 
-        // const div = document.createElement('div');
-        // div.innerHTML = response.html;
-        // document.body.appendChild(div);
-        // const container = div.querySelector('.rst-versions');
-        // div.querySelector('.rst-current-version').addEventListener('click', function() {
-        //     const classes = container.className.split(' ');
-        //     const index = classes.indexOf('shift-up');
-        //     if (index === -1) {
-        //         classes.push('shift-up');
-        //     } else {
-        //         classes.splice(index, 1);
-        //     }
-        //     container.className = classes.join(' ');
-        // });
-
         const div = document.createElement('div');
-        div.innerHTML = response.html
-        console.log(response.html)
+        div.innerHTML = response.html;
         document.body.appendChild(div);
-        var versions = JSON.parse(response.html);
+        const container = div.querySelector('.rst-versions');
+        div.querySelector('.rst-current-version').addEventListener('click', function() {
+            const classes = container.className.split(' ');
+            const index = classes.indexOf('shift-up');
+            if (index === -1) {
+                classes.push('shift-up');
+            } else {
+                classes.splice(index, 1);
+            }
+            container.className = classes.join(' ');
+        });
 
-        var realVersion = versions.find(function(i) {
-        return i.version === "latest" ||
-                i.aliases.includes("latest");
-        }).version;
+    //     const div = document.createElement('div');
+    //     div.innerHTML = response.html
+    //     console.log(response.html)
+    //     document.body.appendChild(div);
+    //     var versions = JSON.parse(response.html);
 
-        var select = makeSelect(versions.map(function(i) {
-            return {text: i.title, value: i.version};
-          }), realVersion);
-          select.addEventListener("change", function(event) {
-            window.location.href = ABS_BASE_URL + "/../" + this.value;
-          });
+    //     var realVersion = versions.find(function(i) {
+    //     return i.version === "latest" ||
+    //             i.aliases.includes("latest");
+    //     }).version;
+
+    //     var select = makeSelect(versions.map(function(i) {
+    //         return {text: i.title, value: i.version};
+    //       }), realVersion);
+    //       select.addEventListener("change", function(event) {
+    //         window.location.href = ABS_BASE_URL + "/../" + this.value;
+    //       });
       
-          var container = document.createElement("div");
-          container.id = "version-selector";
-          container.className = "md-header-nav";
-          container.appendChild(select);
-          var header = document.querySelector(".md-header-nav > .md-header-nav__title");
-    header.parentNode.insertBefore(container, header.nextSibling);
+    //       var container = document.createElement("div");
+    //       container.id = "version-selector";
+    //       container.className = "md-header-nav";
+    //       container.appendChild(select);
+    //       var header = document.querySelector(".md-header-nav > .md-header-nav__title");
+    // header.parentNode.insertBefore(container, header.nextSibling);
 
     }
     
