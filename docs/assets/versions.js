ef498d18de8a384572c99aecdf2d6ca12e8fb36b
Regina Scott <rescott@redhat.com>
2021-01-21 09:32:51 -0500

    integration with custom styling

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -1,20 +1,47 @@
 setTimeout(function() {
     const callbackName = 'callback_' + new Date().getTime();
     window[callbackName] = function (response) {
+
+        // const div = document.createElement('div');
+        // div.innerHTML = response.html;
+        // document.body.appendChild(div);
+        // const container = div.querySelector('.rst-versions');
+        // div.querySelector('.rst-current-version').addEventListener('click', function() {
+        //     const classes = container.className.split(' ');
+        //     const index = classes.indexOf('shift-up');
+        //     if (index === -1) {
+        //         classes.push('shift-up');
+        //     } else {
+        //         classes.splice(index, 1);
+        //     }
+        //     container.className = classes.join(' ');
+        // });
+
         const div = document.createElement('div');
-        div.innerHTML = response.html;
+        div.innerHTML = response.html
+        console.log(response.html)
         document.body.appendChild(div);
-        const container = div.querySelector('.rst-versions');
-        div.querySelector('.rst-current-version').addEventListener('click', function() {
-            const classes = container.className.split(' ');
-            const index = classes.indexOf('shift-up');
-            if (index === -1) {
-                classes.push('shift-up');
-            } else {
-                classes.splice(index, 1);
-            }
-            container.className = classes.join(' ');
-        });
+        var versions = JSON.parse(response.html);
+
+        var realVersion = versions.find(function(i) {
+        return i.version === "latest" ||
+                i.aliases.includes("latest");
+        }).version;
+
+        var select = makeSelect(versions.map(function(i) {
+            return {text: i.title, value: i.version};
+          }), realVersion);
+          select.addEventListener("change", function(event) {
+            window.location.href = ABS_BASE_URL + "/../" + this.value;
+          });
+      
+          var container = document.createElement("div");
+          container.id = "version-selector";
+          container.className = "md-header-nav";
+          container.appendChild(select);
+          var header = document.querySelector(".md-header-nav > .md-header-nav__title");
+    header.parentNode.insertBefore(container, header.nextSibling);
+
     }
     
     var link = document.createElement('link');
@@ -22,9 +49,37 @@ setTimeout(function() {
     link.href = 'https://assets.readthedocs.org/static/css/badge_only.css';
     document.getElementsByTagName('head')[0].appendChild(link);
 
+    // RTD version
+    var versionRTDLink = document.createElement('link');
+    versionRTDLink.rel='stylesheet';
+    versionRTDLink.href = 'assets/versions.css';
+    document.getElementsByTagName('head')[0].appendChild(versionRTDLink);
+
+    // my version
+    var versionCSSLink = document.createElement('link');
+    versionCSSLink.rel='stylesheet';
+    versionCSSLink.href = '../css/version-select.css';
+    document.getElementsByTagName('head')[0].appendChild(versionCSSLink);
+
+    var versionWarningCSSLink = document.createElement('link');
+    versionWarningCSSLink.rel='stylesheet';
+    versionWarningCSSLink.href = '../stylesheets/extra.css';
+    document.getElementsByTagName('head')[0].appendChild(versionWarningCSSLink);
+
+    var versionScript = document.createElement('script');
+    versionScript.src = '../js/version-select.js';
+    document.getElementsByTagName('head')[0].appendChild(versionScript);
+
+    var versionWarningScript = document.createElement('script');
+    versionWarningScript.src ='../javascripts/extra.js';
+    document.getElementsByTagName('head')[0].appendChild(versionWarningScript);
+
     var script = document.createElement('script');
     script.src = 'https://argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
         'callback=' + callbackName + '&project=argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
     
     document.getElementsByTagName('head')[0].appendChild(script); 
-}, 50);
+
+    console.log("version is " + 'https://argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
+    'callback=' + callbackName + '&project=argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA']).version)
+}, 0);
---
+++ docs/javascripts/extra.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", window.location + "assets/versions.json");
+  xhr.onload = function() {
+    var versions = JSON.parse(this.responseText);
+    latest_version = ""
+    for(id in versions)
+        if(versions[id]["aliases"].length > 0 && versions[id]["aliases"].includes("latest")) 
+            latest_version = "/" + versions[id].version + "/"
+    if(!window.location.pathname.includes("/latest/") && (latest_version.length > 0 && !window.location.pathname.includes(latest_version)))
+        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Argo CD, <a href='https://argoproj.github.io/argo-cd/'>click here to go to the latest version</a></div>"
+  };
+  xhr.send();
+}); 
