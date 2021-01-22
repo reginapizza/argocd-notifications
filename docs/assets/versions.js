fc61945e3e2848cc8a92462ae7bf320997c8af56
Regina Scott <rescott@redhat.com>
2021-01-21 22:18:52 -0500

    .

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -83,5 +83,5 @@ setTimeout(function() {
     document.getElementsByTagName('head')[0].appendChild(script); 
 
     // console.log("version is " + (window['READTHEDOCS_DATA']).version)
-    console.log("RTD Data is " + (window['READTHEDOCS_DATA']))
+    console.log("RTD data is " + (window['READTHEDOCS_DATA']))
 }, 0);
