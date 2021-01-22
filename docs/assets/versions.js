fa04a11bce119cd64835fb94a3eaf56450b35e15
Regina Scott <rescott@redhat.com>
2021-01-22 09:06:52 -0500

    .

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -85,3 +85,76 @@ setTimeout(function() {
     // console.log("version is " + (window['READTHEDOCS_DATA']).version)
     console.log("RTD data is " + (window['READTHEDOCS_DATA']))
 }, 0);
+
+window.addEventListener("DOMContentLoaded", function() {
+    var ex = new RegExp("/?assets/version-select.css$");
+    var sheet = document.querySelector('link[href$="version-select.css"]');
+  
+    var ABS_BASE_URL = sheet.href.replace(ex, "");
+    var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
+  
+    function makeSelect(options, selected) {
+      var select = document.createElement("select");
+      select.classList.add("form-control");
+  
+      options.forEach(function(i) {
+        var option = new Option(i.text, i.value, undefined,
+                                i.value === selected);
+        select.add(option);
+      });
+  
+      return select;
+    }
+  
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("GET", ABS_BASE_URL + "/../versions.json");
+    // xhr.onload = function() {
+    //   var versions = JSON.parse(this.responseText);
+  
+    //   var realVersion = versions.find(function(i) {
+    //     return i.version === CURRENT_VERSION ||
+    //            i.aliases.includes(CURRENT_VERSION);
+    //   }).version;
+  
+    var versions = (window['READTHEDOCS_DATA'])
+  
+    var select = makeSelect(versions.map(function(i) {
+      return {text: i.title, value: i.version};
+    }), realVersion);
+    select.addEventListener("change", function(event) {
+      window.location.href = ABS_BASE_URL + "/../" + this.value;
+    });
+  
+    var container = document.createElement("div");
+    container.id = "version-selector";
+    container.className = "md-header-nav";
+    container.appendChild(select);
+  
+    var header = document.querySelector(".md-header-nav > .md-header-nav__title");
+    header.parentNode.insertBefore(container, header.nextSibling);
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", window.location + "assets/versions.json");
+// xhr.onload = function() {
+//   var versions = JSON.parse(this.responseText);
+//   latest_version = ""
+//   for(id in versions)
+//       if(versions[id]["aliases"].length > 0 && versions[id]["aliases"].includes("latest")) 
+//           latest_version = "/" + versions[id].version + "/"
+//   if(!window.location.pathname.includes("/latest/") && (latest_version.length > 0 && !window.location.pathname.includes(latest_version)))
+//       document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Argo CD, <a href='https://argoproj.github.io/argo-cd/'>click here to go to the latest version</a></div>"
+// };
+// xhr.send();
+
+// console.log("version in extra.js is " + (window['READTHEDOCS_DATA']).version)
+
+    if ((window['READTHEDOCS_DATA']).version !== "latest") {
+        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the docs for a previous version of Argo CD, <a href='https://argoproj.github.io/argo-cd/'>click here to go to the latest version</a></div>"
+    }
+
+    if ((window['READTHEDOCS_DATA']).version === "latest") {
+        document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>Yay you're on the latest version of docs! <a href='https://argoproj.github.io/argo-cd/'>click here to go to the latest version</a></div>"
+    }
+}); 
