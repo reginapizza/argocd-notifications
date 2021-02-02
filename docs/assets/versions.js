a772e8e7f32d133d6a15b08f4636c1ea1eaaa2dc
Regina Scott <rescott@redhat.com>
2021-02-02 09:44:18 -0500

    .

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -1,34 +1,36 @@
 setTimeout(function() {
-    const callbackName = 'callback_' + new Date().getTime();
-    window[callbackName] = function (response) {
-      const div = document.createElement('div');
-      div.innerHTML = response.html;
-      document.querySelector(".md-header-nav > .md-header-nav__title").appendChild(div);
-      const container = div.querySelector('.rst-versions');
-      div.querySelector('.rst-current-version').addEventListener('click', function() {
-          const classes = container.className.split(' ');
-          const index = classes.indexOf('shift-up');
-          if (index === -1) {
-              classes.push('shift-up');
-          } else {
-              classes.splice(index, 1);
-          }
-          container.className = classes.join(' ');
-      });
-    }
-  
-    var CSSLink = document.createElement('link');
-    CSSLink.rel='stylesheet';
-    CSSLink.href = '/assets/versions.css';
-    document.getElementsByTagName('head')[0].appendChild(CSSLink);
-  
-    var script = document.createElement('script');
-    script.src = 'https://regina-argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
-        'callback=' + callbackName + '&project=regina-argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
-  
-    document.getElementsByTagName('head')[0].appendChild(script); 
-  
-    console.log("RTD data is " + JSON.stringify((window['READTHEDOCS_DATA'])))
+    div.querySelector('.rst-current-version').addEventListener('change', (event) => {
+        const callbackName = 'callback_' + new Date().getTime();
+        window[callbackName] = function (response) {
+        const div = document.createElement('div');
+        div.innerHTML = response.html;
+        document.querySelector(".md-header-nav > .md-header-nav__title").appendChild(div);
+        const container = div.querySelector('.rst-versions');
+        div.querySelector('.rst-current-version').addEventListener('click', function() {
+            const classes = container.className.split(' ');
+            const index = classes.indexOf('shift-up');
+            if (index === -1) {
+                classes.push('shift-up');
+            } else {
+                classes.splice(index, 1);
+            }
+            container.className = classes.join(' ');
+        });
+        }
+    
+        var CSSLink = document.createElement('link');
+        CSSLink.rel='stylesheet';
+        CSSLink.href = '/assets/versions.css';
+        document.getElementsByTagName('head')[0].appendChild(CSSLink);
+    
+        var script = document.createElement('script');
+        script.src = 'https://regina-argocd-notifications.readthedocs.io/_/api/v2/footer_html/?'+
+            'callback=' + callbackName + '&project=regina-argocd-notifications&page=&theme=mkdocs&format=jsonp&docroot=docs&source_suffix=.md&version=' + (window['READTHEDOCS_DATA'] || { version: 'latest' }).version;
+    
+        document.getElementsByTagName('head')[0].appendChild(script); 
+    
+        console.log("RTD data is " + JSON.stringify((window['READTHEDOCS_DATA'])))
+    });
   }, 0);
 
 // VERSION WARNINGS
